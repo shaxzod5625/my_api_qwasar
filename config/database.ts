63346a9a7fd9a797297ce52001884189ee176883
Shaxzod <shaxzodzor007@gmail.com>
2022-08-15 14:22:57 +0500

    changed database.ts

--- config/database.ts
+++ config/database.ts
@@ -8,8 +8,8 @@
 import Env from '@ioc:Adonis/Core/Env'
 import { DatabaseConfig } from '@ioc:Adonis/Lucid/Database'
 
-// import Url from 'url-parse'
-// const CLEARDB_DATABASE_URL = new Url(Env.get('CLEARDB_DATABASE_URL'))
+import Url from 'url-parse'
+const CLEARDB_DATABASE_URL = new Url(Env.get('CLEARDB_DATABASE_URL'))
 
 const databaseConfig: DatabaseConfig = {
   /*
@@ -36,32 +36,32 @@ const databaseConfig: DatabaseConfig = {
     | npm i mysql
     |
     */
-    // mysql: {
-    //   client: 'mysql',
-    //   connection: {
-    //     host: CLEARDB_DATABASE_URL.host as string,
-    //     port: Number(''),
-    //     user: CLEARDB_DATABASE_URL.username as string,
-    //     password: CLEARDB_DATABASE_URL.password as string,
-    //     database: CLEARDB_DATABASE_URL.pathname.substr(1) as string
-    //   },
-    //   healthCheck: false,
-    // },
     mysql: {
       client: 'mysql',
       connection: {
-        host: Env.get('MYSQL_HOST'),
-        port: Env.get('MYSQL_PORT'),
-        user: Env.get('MYSQL_USER'),
-        password: Env.get('MYSQL_PASSWORD', ''),
-        database: Env.get('MYSQL_DB_NAME'),
-      },
-      migrations: {
-        naturalSort: true,
+        host: CLEARDB_DATABASE_URL.host as string,
+        port: Number(''),
+        user: CLEARDB_DATABASE_URL.username as string,
+        password: CLEARDB_DATABASE_URL.password as string,
+        database: CLEARDB_DATABASE_URL.pathname.substr(1) as string
       },
       healthCheck: false,
-      debug: false,
     },
+    // mysql: {
+    //   client: 'mysql',
+    //   connection: {
+    //     host: Env.get('MYSQL_HOST'),
+    //     port: Env.get('MYSQL_PORT'),
+    //     user: Env.get('MYSQL_USER'),
+    //     password: Env.get('MYSQL_PASSWORD', ''),
+    //     database: Env.get('MYSQL_DB_NAME'),
+    //   },
+    //   migrations: {
+    //     naturalSort: true,
+    //   },
+    //   healthCheck: false,
+    //   debug: false,
+    // },
   }
 }
 
